6d17397459c0d3b17854655c9f78149be3f8c19c
Dimonchik <sudjadred@live.com>
2022-08-29 13:26:52 +0200

    Просто Мария

---
+++ Hello_Masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Что ты такое: ");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Эгэгэй, ведь это у нас Мария");
+}
+else
+{
+    Console.WriteLine("Просто привет, ");
+    Console.WriteLine(username);
+}
